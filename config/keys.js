f4ce505b95b4f2cea99bd3801a64f25fd11290a5
Jitender  Sharma <jitendersharma@Jitenders-MacBook-Air.local>
2023-12-17 10:08:07 +0530

    feat(security and logging): implement security and logging middleware implement security middleware like cors, api-rate-limit implement logging with morgain
    #Closes #3

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
     port: process.env.PORT || 4000,
     nodeEnv: process.env.NODE_ENV || 'development',
-    mongoURI: process.env.MONGO_URI || 'mongodb+srv://user97:user97@cluster0.hbcz8.mongodb.net/frequentResearch?retryWrites=true&w=majority'
+    mongoURI: process.env.MONGO_URI || 'mongodb+srv://user97:user97@cluster0.hbcz8.mongodb.net/frequentResearch?retryWrites=true&w=majority',
+    allowHost: process.env.ALLOW_HOST || 'http://prod-example.com'
 }
--- server.js
+++ server.js
@@ -1,18 +1,45 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 require("dotenv").config();
 const config = require("./config/keys");
 const routes = require("./routes");
+const keys = require("./config/keys");
 
+require("./config/db")(); // connect to db
 
+const server = express(); // init server
 
-// connect to db
-require("./config/db")();
+const allowedOrigins = [keys.allowHost];
+const allowedMethods = ['GET'];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (keys.nodeEnv === 'development') {
+            callback(null, true);
+        } else {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        }
+    },
+    methods: allowedMethods,
+};
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: 'Too many requests from this IP, please try again later.',
+});
 
-const server = express();
-
-
+server.use(cors(corsOptions)); // CORS security
+server.use(limiter); // rate limit
 server.use(bodyParser.json());
+if (keys.nodeEnv === 'development') {
+    server.use(morgan('dev')); // API log
+}
 
 server.use('/', routes);
 
